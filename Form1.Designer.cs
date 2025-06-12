2bef7d040692e6c40051cb4044602cd628bad16e
YuJ-DSG <1730897017@qq.com>
2025-06-12 21:38:57 +0800

    Update Form1.Designer.cs
    远程删除了两个按钮

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,28 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(168, 144);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "OK";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(455, 143);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "CANCEL";
-            this.button2.UseVisualStyleBackColor = true;
+
             // 
             // Form1
             // 
